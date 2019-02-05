bb2906dcdc8ac3cf2bd08a5d0c6184b0944198ec
Jerrol Krause <jerrolkrause@gmail.com>
2019-02-05 15:20:34 -0800

    Coloring

--- src/app/features/map-mapbox/services/map-objects.service.ts
+++ src/app/features/map-mapbox/services/map-objects.service.ts
@@ -171,7 +171,7 @@ export class MapObjectsService {
         //   * Blue, 20px circles when point count is less than 100
         //   * Yellow, 30px circles when point count is between 100 and 750
         //   * Pink, 40px circles when point count is greater than or equal to 750
-        'circle-color': ['step', ['get', 'point_count'], '#51bbd6', 100, '#f1f075', 750, '#f28cb1'],
+        'circle-color': ['step', ['get', 'point_count'], '#ead499', 100, '#c7ab5f', 750, '#9a7c29'],
         'circle-radius': ['step', ['get', 'point_count'], 20, 100, 30, 750, 40],
       },
     });
